6c02c04ccc509bf3de5746b4675b78d20d554311
Nathan Oelke <oelke047@umn.edu>
2018-01-28 11:57:26 -0600

    Updated base.py

--- main/views/base.py
+++ main/views/base.py
@@ -4,9 +4,43 @@ blueprint = Blueprint("base", __name__)
 
 @blueprint.route("/result.html", methods=["GET"])
 def result():
-    #with open("", "w+") as csvfile:
+    with open("/home/nathan/Documents/Minnehack-2018/data/database.csv", "a") as csvfile:
+        row = ""
+        args = request.args
+        print(args)
+        for key, value in args.items():
+            if key == "sex":
+                if value == "male":
+                    row += "1,0,0,"
+                elif value == "female":
+                    row += "0,1,0,"
+                else:
+                    row += "0,0,1,"
 
-    return "Result"
+            elif key == "position":
+                #row += value + ","
+                pass
+
+            elif key == "equipment":
+                if value == "free-weights":
+                    row += "1,0,0,0,"
+                elif value == "machines":
+                    row += "0,1,0,0,"
+                elif value == "both-equipment":
+                    row += "0,0,1,0,"
+                else:
+                    row += "0,0,0,0,"
+
+            elif value == "":
+                row += "0,"
+
+            else:
+                row += value + ","
+
+        row += "\n"
+        csvfile.write(row)
+        return render_template("result.html", predicted_missed_games=5)
+
 
 
 @blueprint.route("/")
