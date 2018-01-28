b48d09a9cc509736d5ad91a5669e0acba262c9d3
Juan David Garrido <juandagarrido@hotmail.com>
2018-01-28 05:42:05 -0600

    Added webpage to main folder

--- main/views/base.py
+++ main/views/base.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template
 
 blueprint = Blueprint("base", __name__)
 
-
 @blueprint.route("/")
 def index():
-    return "Hello"
+    return render_template('index.html')
