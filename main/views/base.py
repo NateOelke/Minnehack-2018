45a6deea84aab35076f65ae4c0c61c9636b601cd
Nathan Oelke <oelke047@umn.edu>
2018-01-28 09:33:12 -0600

    Added database stuff

--- main/views/base.py
+++ main/views/base.py
@@ -2,6 +2,13 @@ from flask import Flask, Blueprint, render_template, request
 
 blueprint = Blueprint("base", __name__)
 
+@blueprint.route("/result.html", methods=["GET"])
+def result():
+    #with open("", "w+") as csvfile:
+
+    return "Result"
+
+
 @blueprint.route("/")
 def index():
     return render_template('index.html')
