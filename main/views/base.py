035cec06c230bbc844dedd81e04c3d49a579111a
Dingwei Deng <Devin.deng.azn@gmail.com>
2018-01-28 05:51:03 -0600

    keep static changes

--- main/views/base.py
+++ main/views/base.py
@@ -4,8 +4,4 @@ blueprint = Blueprint("base", __name__)
 
 @blueprint.route("/")
 def index():
-<<<<<<< HEAD
-    render_template("index.html")
-=======
     return render_template('index.html')
->>>>>>> 8cf2f599db6c0316466a3657ccfdde883b5523dc
